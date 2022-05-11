465fcd722ecf708f7aee308aff7fd5c91d682c5e
CosminaX <cosminaalupoaei@yahoo.com>
2022-05-11 13:56:38 +0300

    Render object

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const actors = {
+    name: "Leonardo Dicaprio",
+    score: 10,
+    hobbies: "Music and dancing naked in the rain",
+    description: "He is a good guy with a thick mustache"
+  }
+  const listItem = Object.keys(actors);
+  console.log(listItem);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Display the object’s information in jsx using <ul> and <li> tags. */}
+      <ul>
+        <li>{actors.name}</li>
+        <li>{actors.score}</li>
+        <li>{actors.hobbies}</li>
+        <li>{actors.description}</li>
+      </ul>
+      {/* Display the object’s information in jsx, imagining that you don’t know its properties in advance. */}
+      <ul>
+        {listItem.map((item, index) => (
+          <li key={index}>
+            {item}: {actors[item]}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
